420233dfccdf47af1f06d35e0573faae8e7dc909
Phillister-yekebedi <otienoyekebedi@gmail.com>
2023-05-01 09:03:56 +0300

    assess

---
+++ program.js
@@ -0,0 +1,79 @@
+//1. Create a function called getAvailableBooks that returns an array of all available
+//books.
+const books =  { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,isAvailable: true }
+
+function getAvailableBooks(){
+    let empty =[]
+    return empty
+
+}
+const book =  ['The Catcher in the Rye','Great Expectations','Crime and Punishment','The Great Gatsby','The Odyssey']
+console.log(book)
+
+
+
+//2. Create a function getBooksByAuthor that takes an author's name as an argument and
+//returns an array of all books by that author.
+
+
+function getBooksByAuthor(name){
+    let books = []
+    books.array.forEach (funnction(name)) 
+        return name
+        if(books )
+    };
+
+let name = 'Charles Dickens'
+
+console.log (getBooksByAuthor(name))
+
+//3. Create a function addNewBook that takes a book object as an argument and adds it
+//to the library, ensuring that the new book has all required properties (title, author,
+//publicationYear, and isAvailable).
+const author =[title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true ] 
+function addNewBook(book){
+    let bookObject = {}
+
+
+
+
+
+
+
+
+
+
+}
+//4. Create a function checkoutBook that takes a book title as an argument and changes
+//the book's isAvailable property to false. If the book is not found in the library, the
+//function should return a message indicating that the book is not available.
+
+function checkout (booktitle){
+    let array = []
+    for(let i = 0;1< booktitle.length;i++){
+        return false
+    }else{
+        return("book to available")
+    }
+
+}
+
+//5. Create a function returnBook that takes a book title as an argument and changes the
+//book's isAvailable property to true. If the book is not found in the library, the function
+//should return a message indicating that the book does not belong to the library.
+function returnBook(book){
+    let num = []
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
